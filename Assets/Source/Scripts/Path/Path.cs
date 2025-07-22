ae9316708a5c56912ef56567ef97b19e3ce6e8c8
forcepusher <bionitsoup@gmail.com>
2025-07-22 18:49:49 +0700

    Add an exception for safety

--- Assets/Source/Scripts/Path/Path.cs
+++ Assets/Source/Scripts/Path/Path.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +9,12 @@ namespace BananaParty.Arch.Samples
         [SerializeField]
         public List<Transform> Waypoints;
 
-        public PathProgress StartFollowing() => new(this);
+        public PathProgress StartFollowing()
+        {
+            if (Waypoints.Count < 2)
+                throw new InvalidOperationException($"Cannot {nameof(StartFollowing)}. {nameof(Path)} must have at least two {nameof(Waypoints)}.");
+
+            return new PathProgress(this);
+        } 
     }
 }
