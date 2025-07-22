1047aabc0d5afa34a1cce391b5a03733d99b24f5
forcepusher <bionitsoup@gmail.com>
2025-07-23 00:04:20 +0700

    Work on shooting logic

--- Assets/Source/Scripts/Towers/ArcherTower.cs
+++ Assets/Source/Scripts/Towers/ArcherTower.cs
@@ -5,11 +5,25 @@ namespace BananaParty.Arch.Samples
     public class ArcherTower : MonoBehaviour
     {
         [SerializeField]
+        private MonsterReferenceList _monsterReferenceList;
+
+        [SerializeField]
         private ArrowProjectile _arrowProjectile;
 
         private void FixedUpdate()
         {
-            
+            // TODO: wrong, it should lock on targets
+            float nearestMonsterDistance = float.PositiveInfinity;
+            Monster nearestMonster = null;
+            foreach (Monster monster in _monsterReferenceList.Values)
+            {
+                float monsterDistance = Vector3.Distance(transform.position, monster.transform.position);
+                if (monsterDistance < nearestMonsterDistance)
+                {
+                    nearestMonsterDistance = monsterDistance;
+                    nearestMonster = monster;
+                }
+            }
         }
     }
 }
