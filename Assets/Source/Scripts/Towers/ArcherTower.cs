0f470bf47295faa1aeb8f974c33400a1368bbbf6
forcepusher <bionitsoup@gmail.com>
2025-07-22 23:10:46 +0700

    Refactor, add archer tower

---
+++ Assets/Source/Scripts/Towers/ArcherTower.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace BananaParty.Arch.Samples
+{
+    public class ArcherTower : MonoBehaviour
+    {
+        private void FixedUpdate()
+        {
+            
+        }
+    }
+}
