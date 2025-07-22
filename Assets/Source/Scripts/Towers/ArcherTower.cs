efacd62d97f10cec7ae792b218dc6622c76cbba5
forcepusher <bionitsoup@gmail.com>
2025-07-22 23:19:46 +0700

    Add arrow projectile

--- Assets/Source/Scripts/Towers/ArcherTower.cs
+++ Assets/Source/Scripts/Towers/ArcherTower.cs
@@ -4,6 +4,9 @@ namespace BananaParty.Arch.Samples
 {
     public class ArcherTower : MonoBehaviour
     {
+        [SerializeField]
+        private ArrowProjectile _arrowProjectile;
+
         private void FixedUpdate()
         {
             
---
+++ Assets/Source/Scripts/Towers/ArrowProjectile.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace BananaParty.Arch.Samples
+{
+    public class ArrowProjectile : MonoBehaviour
+    {
+
+    }
+}
