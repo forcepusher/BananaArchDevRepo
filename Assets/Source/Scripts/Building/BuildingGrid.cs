ddf951623240e7d2fd0d0c376640d37d1d50deb6
forcepusher <bionitsoup@gmail.com>
2025-07-24 19:37:29 +0700

    Add click interception

--- Assets/Source/Scripts/Building/BuildingGrid.cs
+++ Assets/Source/Scripts/Building/BuildingGrid.cs
@@ -1,14 +1,23 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.Tilemaps;
 
 namespace BananaParty.Arch.TowerDefenseSample
 {
     public class BuildingGrid : MonoBehaviour
     {
-        private void Awake()
+        [SerializeField]
+        private Tilemap _groundTilemap;
+        [SerializeField]
+        private Tilemap _roadTilemap;
+        [SerializeField]
+        private Tilemap _noBuildZoneTilemap;
+
+        public void OnGroundClickOrTap(BaseEventData eventData)
         {
-            Tilemap derp;
-            
+            PointerEventData pointerEventData = eventData as PointerEventData;
+            Debug.Log(pointerEventData.position);
+            //Debug.Log("Click");
         }
     }
 }
