361daf5a05d1d526e16bbe9aa581a0bfc7b50e6b
forcepusher <bionitsoup@gmail.com>
2025-07-24 21:11:24 +0700

    Work on building

--- Assets/Source/Scripts/Building/BuildGrid.cs
+++ Assets/Source/Scripts/Building/BuildGrid.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Tilemaps;
@@ -8,15 +9,24 @@ namespace BananaParty.Arch.TowerDefenseSample
     public class BuildGrid : MonoBehaviour, IPointerClickHandler
     {
         [SerializeField]
+        private Grid _grid;
+
+        [SerializeField]
         private Tilemap _groundTilemap;
         [SerializeField]
         private Tilemap _roadTilemap;
         [SerializeField]
         private Tilemap _noBuildZoneTilemap;
 
+        [SerializeField]
+        private ArcherTower _archerTowerPrefab;
+
+        private readonly HashSet<Vector3Int> _busyTiles = new();
+
         public void OnPointerClick(PointerEventData pointerClickEventData)
         {
-            if (!pointerClickEventData.pointerCurrentRaycast.gameObject.TryGetComponent(out Tilemap clickedTilemap))
+            RaycastResult clickRaycastResult = pointerClickEventData.pointerCurrentRaycast;
+            if (!clickRaycastResult.gameObject.TryGetComponent(out Tilemap clickedTilemap))
                 return;
 
             if (clickedTilemap == _roadTilemap)
@@ -33,6 +43,16 @@ namespace BananaParty.Arch.TowerDefenseSample
 
             if (clickedTilemap == _groundTilemap)
             {
+                Vector3Int gridSpacePosition = _grid.WorldToCell(clickRaycastResult.worldPosition);
+
+                if (_busyTiles.Contains(gridSpacePosition))
+                {
+                    Debug.Log("Can't build on top of other buildings.");
+                    return;
+                }
+
+                GameObject.Instantiate(_archerTowerPrefab, _grid.GetCellCenterWorld(gridSpacePosition), Quaternion.identity);
+                _busyTiles.Add(gridSpacePosition);
                 Debug.Log(pointerClickEventData.position);
             }
             else
