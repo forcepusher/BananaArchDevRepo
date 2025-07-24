e497ea29a17664ecadf5f8df05ad6afc78fc471d
forcepusher <bionitsoup@gmail.com>
2025-07-24 17:26:33 +0700

    Add building grid script

---
+++ Assets/Source/Scripts/Building/BuildingGrid.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace BananaParty.Arch.TowerDefenseSample
+{
+    public class BuildingGrid : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Tilemap derp;
+            
+        }
+    }
+}
